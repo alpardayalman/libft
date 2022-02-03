f896f21a84b5f36550bfe7b8c76dfb1347a52605
Alp arda Yalman <ayalman@k2m07s09.42kocaeli.com.tr>
2022-02-03 03:58:11 +0300

    ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ayalman <ayalman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 21:37:56 by ayalman           #+#    #+#             */
-/*   Updated: 2022/02/02 22:58:55 by ayalman          ###   ########.fr       */
+/*   Updated: 2022/02/03 03:57:01 by ayalman          ###   ########.Tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,28 @@
 char	*ft_substr(const char *source, unsigned int start, size_t len)
 {
 	size_t	index;
+	size_t	j;
 	char	*ptr;
 
-	index = 0;
-	ptr = malloc ((len + 1) * sizeof(char));
-	if (!ptr)
-		return (NULL);
+	ptr = NULL;
 	if (source)
 	{
 		if (start >= ft_strlen(source))
-			start -= ft_strlen(source);
+			start = ft_strlen(source) % start;
 		index = 0;
 		while (index < len && source[(size_t)start + index] != '\0')
-		{
-			ptr[index] = source[(size_t)start + index];
 			index++;
+		j = 0;
+		ptr = malloc(sizeof(char) * index + 1);
+		if (!ptr)
+			return (NULL);
+		while (j < index)
+		{
+			ptr[j] = source[(size_t)start + j];
+			j++;
 		}
 		ptr[index] = '\0';
+		return (ptr);
 	}
-	return (ptr);
+	return (NULL);
 }
