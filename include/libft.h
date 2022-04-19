d42034b907104fafb077cfd866057f4ee326280f
Alp A. Yalman <82611850+alpardayalman@users.noreply.github.com>
2022-04-19 15:36:38 +0300

    Update libft.h

--- include/libft.h
+++ include/libft.h
@@ -12,7 +12,7 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
+# ------------------------------ Libs --------------------------------
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdbool.h>
@@ -21,22 +21,25 @@
 # include <fcntl.h>
 # include <signal.h>
 
+# ------------------------------ Colors ------------------------------
 # define ANSI_RED		"\x1b[31m"
 # define ANSI_BLUE		"\x1b[34m"
 # define ANSI_GREEN		"\x1b[32m"
 # define ANSI_YELLOW	"\x1b[33m"
 # define ANSI_RESET		"\x1b[0m"
 
+# ------------------------------ Buffer ------------------------------
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 4
 # endif
 
+# ------------------------------ Struct ------------------------------
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
-
+# ------------------------------ 1. Part ------------------------------
 int		ft_isalpha(int a);
 bool	ft_str_isalpha(char *str);
 int		ft_isalnum(int a);
@@ -70,7 +73,7 @@ char	*ft_strnstr(const char *bigy, const char *mini, size_t n);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strchr(const char *s, int c);
 
-//2.Part
+# ------------------------------ 2.Part ------------------------------
 void	ft_putchar_fd(char a, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
@@ -86,7 +89,7 @@ char	*ft_itoa(int n);
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
-//3.Bonus
+# ------------------------------ Bonus ------------------------------
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
@@ -97,7 +100,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
-//Math
+# ------------------------------ Math --------------------------------
 void	ft_quick(int *array, int low, int high);
 int		ft_power(int nb, int power);
 bool	ft_isprime(int nb);
@@ -106,7 +109,7 @@ int		ft_factorial(int nb);
 int		ft_abs(int a);
 int		ft_comdiv(int a, int b);
 
-//printf
+# ------------------------------ Printf ------------------------------
 void	ft_dec2hexhigh(long int decimalNumber);
 int		ft_printf(const char *str, ...);
 void	ft_dec2hexlow(long int decimalNumber);
@@ -125,7 +128,7 @@ int		ft_int_format(va_list *args, char c);
 int		ft_hex_format(va_list *args, char c);
 int		ft_unsigned_format(va_list *args);
 
-//get next line
+# ------------------------------ GNL ------------------------------
 char	*get_next_line(int fd);
 char	*ft_read_to_left_str(int fd, char *left_str);
 char	*ft_get_line(char *left_str);
